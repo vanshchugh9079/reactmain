3adce649a008a1d61f39a109c06772827cf17be9
vanshchugh9079 <vanshchugh9079@gmail.com>
2023-10-11 10:10:21 +0530

    react1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,171 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
+let A=(props)=>{
+    // console.log(props.children);
+    return(<h1>
+    my name is vansh+{props.name}+{props.children}
+    </h1>
+    )
+}
+class B extends React.Component
+{
+    render(){
+        return(<span>
+         <A name="vansh">
+        <h1>watassapp</h1>
+        </A>
+        </span>
+        )
+}
+}
+let C=()=>{
+    return(<h1>my name is vansh</h1>)
+}
+class D extends React.Component
+{
+   render(){
+    return(<span>
+    ju7 fhrfgjk
+    </span>
+    )
+   }
+}
+let E=()=>{
+    return(<h1>hlo gamer</h1>)
+}
+class F extends React.Component
+{
+render(){
+    return(<h1>hlo gamer</h1>)
+}
+}
+let G=()=>{
+    return(<h1>hlo gamer</h1>)
+}
+class H extends React.Component
+{
+    render(){
+        return(<h1>hlo gamer</h1>)
+    }
+}
+let I=()=>{
+    return(<h1>hlo gamer</h1>)
+}
+class J extends React.Component
+{
+    render(){
+        return(<h1>hlo gamer</h1>)
+    }
+}
+let K=(props)=>{
+    return(<h1>hlo gamer</h1>)
+}
+class L extends React.Component
+{
+    render(){
+        return(<h1>hlo gamer</h1>)
+    }
+}
+let M=(props)=>{
+    return(<h1>hlo gamer</h1>)
+}
+class N extends React.Component
+{
+    render(){
+        return(<h1>hlo gamer</h1>)
+    }
+}
+let O=(props)=>{
+    return(<h1>hlo gamer</h1>)
+}
+class P extends React.Component
+{
+    render(){
+        return(<h1>hlo gamer</h1>)
+    }
+}
+let Q=(props)=>{
+    return(<h1>hlo gamer</h1>)
+}
+class R extends React.Component
+{
+    render(){
+        return(<h1>hlo gamer</h1>)
+    }
+}
+let S=(props)=>{
+    return(<h1>hlo gamer</h1>)
+}
+class T extends React.Component
+{
+    render(){
+        return(<h1>hlo gamer</h1>)
+    }
+}
+let U=(props)=>{
+    return(<span>
+             <V>{props.children}</V>
+        </span>)
+}
+class V extends React.Component
+{
+    render(){
+        return(<span>
+            <h1>hlo gamer</h1>
+            </span>)
+    }
+}
+let W=(props)=>{
+    return(<span>
+        <h1>hlo gamer</h1>
+        </span>)
+}
+class X extends React.Component
+{
+    render(){
+        return(<span>
+        <Y>{this.props.children}</Y>
+        </span>
+        )
+    }
+}
+let Y=(props)=>{
+    return(<span><h1>hlo gamer</h1></span>)
+}
+class Z extends React.Component
+{
+    render(){
+        return(<h1>hlo gamer</h1>)
+}
+}
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <>
+    <B/>
+    <C/>
+    <D/>
+    <E/>
+    <F/>
+    <G/>
+    <H/>
+    <I/>
+    <J/>
+    <K/>
+    <L/>
+    <M/>
+    <N/>
+    <O/>
+    <P/>
+    <Q/>
+    <R/>
+    <S/>
+    <T/>
+    <U/>
+    <V/>
+    <W/>
+    <X/>
+    <Y/>
+    <Z/>
+    </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
